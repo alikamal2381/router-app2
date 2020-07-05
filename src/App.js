94573a7554ef8455ec3c9f9837bc9bfcd2ba5cbd
AliKamal <alikamal2381@gmail.com>
2020-07-06 02:13:58 +0500

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Single from "./pages/Single";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 2
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/single" component={Single} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
